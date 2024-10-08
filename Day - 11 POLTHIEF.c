4d62410d70da8bd6158c38ccbd808756c622b897
Atharvvvvvv <119841131+Atharvvvvvv@users.noreply.github.com>
2024-10-08 20:10:14 +0530

    Day 11 easy problem

---
+++ Day - 11 POLTHIEF.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+    int t,x,y;
+    scanf("%d",&t);
+    while(t--){
+        scanf("%d%d",&x,&y);
+       
+            printf("%d\n",abs (x-y));
+        
+        
+        
+    }
+	// your code goes here
+	return 0;
+}
